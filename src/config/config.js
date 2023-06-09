fe37819d59189c74fc00c1d5638b3c58b764bf69
cvnkt422 <cvnkt422@gmail.com>
2023-06-09 13:48:41 +0530

    adjustments

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-const devUrl = "http://localhost:8081";
+const devUrl = "http://localhost:8080";
 const prodUrl = "http://ec2-52-91-103-2.compute-1.amazonaws.com:8080";
-const url = prodUrl;
+const url = devUrl;
 
 export default { url };
