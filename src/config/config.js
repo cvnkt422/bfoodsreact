561bfe5156753fb64960241f635d0874e43ce286
cvnkt422 <121794859+cvnkt422@users.noreply.github.com>
2023-06-12 20:25:25 +0530

    Update config.js with ec2 public ip

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const devUrl = "http://localhost:8081";
-const prodUrl = "http://ec2-52-91-103-2.compute-1.amazonaws.com:8080";
+const prodUrl = "http://3.211.89.170:8080";
 const url = prodUrl;
 
 export default { url };
